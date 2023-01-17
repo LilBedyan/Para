eccb634ecdcb40c7c4558f0bae3db69cccd10df5
vladi <vladislav.lebedev2018@yandex.ru>
2023-01-17 19:28:41 +0300

    Timer

--- Para/Log_in.Designer.cs
+++ Para/Log_in.Designer.cs
@@ -28,27 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(201, 258);
+            this.button1.Location = new System.Drawing.Point(180, 292);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "Вход";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(167, 112);
+            this.textBox1.Location = new System.Drawing.Point(140, 53);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(193, 37);
@@ -56,7 +62,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(167, 182);
+            this.textBox2.Location = new System.Drawing.Point(140, 123);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(193, 37);
@@ -65,7 +71,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(167, 95);
+            this.label1.Location = new System.Drawing.Point(140, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(129, 16);
             this.label1.TabIndex = 3;
@@ -74,7 +80,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(167, 163);
+            this.label2.Location = new System.Drawing.Point(140, 104);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 16);
             this.label2.TabIndex = 4;
@@ -82,18 +88,56 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(366, 182);
+            this.button2.Location = new System.Drawing.Point(339, 123);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(146, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Показать пароль";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(140, 190);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(193, 50);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(140, 255);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Обновить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(140, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "CAPCHA";
+            // 
             // log_in
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(537, 391);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -103,6 +147,7 @@
             this.Name = "log_in";
             this.Text = "Авторизация";
             this.Load += new System.EventHandler(this.log_in_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +161,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Para/Log_in.cs
+++ Para/Log_in.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Timers;
+using System.Threading;
 
 namespace Para
 {
@@ -20,8 +22,11 @@ namespace Para
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
             button2.Click += new EventHandler(button2_firstClick);
-
+            button1.Click += new EventHandler(button1_1Click);
+            pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
         }
+
+        private string text = String.Empty;
 
         private void log_in_Load(object sender, EventArgs e)
         {
@@ -30,7 +35,7 @@ namespace Para
             textBox1.MaxLength = 50;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_1Click(object sender, EventArgs e)
         {
             var loginUser = textBox1.Text;
             var passUser = textBox2.Text;
@@ -45,7 +50,7 @@ namespace Para
             adapter.SelectCommand = command;
             adapter.Fill(table);
 
-            if (table.Rows.Count == 1)
+            if (table.Rows.Count == 1 && textBox3.Text == this.text)
             {
                 // MessageBox.Show("Авторизация завершена успешно", "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 Form1 frm1 = new Form1();
@@ -54,10 +59,86 @@ namespace Para
                 this.Show();
             }
             else
-                MessageBox.Show("Такого аккаунта не существует", "Аккаунта не существует", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            {
+                MessageBox.Show("Такого аккаунта не существует или неверно введена CAPCHA", "Аккаунта не существует", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
 
+            button1.Click -= new EventHandler(button1_1Click);
+            button1.Click += new EventHandler(button1_2Click);
         }
-        
+        private void button1_2Click(object sender, EventArgs e)
+        {
+            var loginUser = textBox1.Text;
+            var passUser = textBox2.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"SELECT User_name, Password FROM Login WHERE User_name ='{loginUser}' and Password = '{passUser}'";
+
+            SqlCommand command = new SqlCommand(querystring, dataBase.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 1 && textBox3.Text == this.text)
+            {
+                // MessageBox.Show("Авторизация завершена успешно", "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Form1 frm1 = new Form1();
+                this.Hide();
+                frm1.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Такого аккаунта не существует или неверно введена CAPCHA", "Аккаунта не существует", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            button1.Click -= new EventHandler(button1_2Click);
+            button1.Click += new EventHandler(button1_3Click);
+        }
+        private void button1_3Click(object sender, EventArgs e)
+        {
+            var loginUser = textBox1.Text;
+            var passUser = textBox2.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"SELECT User_name, Password FROM Login WHERE User_name ='{loginUser}' and Password = '{passUser}'";
+
+            SqlCommand command = new SqlCommand(querystring, dataBase.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 1 && textBox3.Text == this.text)
+            {
+                // MessageBox.Show("Авторизация завершена успешно", "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Form1 frm1 = new Form1();
+                this.Hide();
+                frm1.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Такого аккаунта не существует или неверно введена CAPCHA", "Аккаунта не существует", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                timer1.Interval = 60000; //Миллисекунды
+                timer1.Tick += timer_Tick;
+                timer1.Start();
+                button1.Enabled = false;
+            }
+
+            button1.Click -= new EventHandler(button1_3Click);
+            //button1.Click += new EventHandler(button1_1Click);
+        }
+
+        void timer_Tick(object sender, System.EventArgs e)
+        {
+            button1.Enabled = true;
+            timer1.Stop();
+        }
+
         private void button2_firstClick(object sender, EventArgs e)
         {
             textBox2.UseSystemPasswordChar = true;
@@ -72,6 +153,63 @@ namespace Para
             button2.Text = "Показать пароль";
             button2.Click += new EventHandler(button2_firstClick);
             button2.Click -= new EventHandler(button2_secondClick);
+        }
+
+        private Bitmap CreateImage(int Width, int Height)
+        {
+            Random rnd = new Random();
+
+            //Создадим изображение
+            Bitmap result = new Bitmap(Width, Height);
+
+            //Вычислим позицию текста
+            int Xpos = rnd.Next(0, Width - 100);
+            int Ypos = rnd.Next(5, Height - 20);
+
+            //Добавим различные цвета
+            Brush[] colors = { Brushes.Black,
+                     Brushes.Red,
+                     Brushes.RoyalBlue,
+                     Brushes.Green };
+
+            //Укажем где рисовать
+            Graphics g = Graphics.FromImage((Image)result);
+
+            //Пусть фон картинки будет серым
+            g.Clear(Color.Gray);
+
+            //Сгенерируем текст
+            text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i < 5; ++i)
+                text += ALF[rnd.Next(ALF.Length)];
+
+            //Нарисуем сгенирируемый текст
+            g.DrawString(text,
+                         new Font("Arial", 15),
+                         colors[rnd.Next(colors.Length)],
+                         new PointF(Xpos, Ypos));
+
+            //Добавим немного помех
+            /////Линии из углов
+            g.DrawLine(Pens.Black,
+                       new Point(0, 0),
+                       new Point(Width - 1, Height - 1));
+            g.DrawLine(Pens.Black,
+                       new Point(0, Height - 1),
+                       new Point(Width - 1, 0));
+            ////Белые точки
+            for (int i = 0; i < Width; ++i)
+                for (int j = 0; j < Height; ++j)
+                    if (rnd.Next() % 20 == 0)
+                        result.SetPixel(i, j, Color.White);
+
+            return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
         }
     }
 }
